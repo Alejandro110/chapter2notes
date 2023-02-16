068e9b565f34d1d3fc447f20baf3a77d12c5056b
2j8e <2j8e@fresnou.org>
2023-02-16 10:30:04 -0800

    updated input2

---
+++ inputs.js
@@ -0,0 +1,10 @@
+const readline =require("readline-sync");
+
+
+let question = readline.query("do you want to hear a joke?\n your answer:");
+console.log(answer);
+
+if (answer) = ("sure")
+
+let question1 = readline.query("de 34\nyour answer:");
+
