b8664c426105b7a015888b606ddc44930061b5ee
2j8e <2j8e@fresnou.org>
2023-02-17 10:29:22 -0800

    updated input2

---
+++ whileloops1.js
@@ -0,0 +1,23 @@
+const readline = require('readline-sync');
+let x=0;
+while (x<26){
+    console.log(x);
+    x++;
+}
+
+let y=0;
+while (y<51){
+    console.log(y);
+    y++;
+}
+let z=10;
+while (z>-11){
+    console.log(z);
+    z--;
+}
+
+let c=0;
+while (c>-51){
+    console.log(c);
+    c--;
+}
