f59368811bf2a01549ed0c9d95c7a2bc4acaefbd
2j8e <2j8e@fresnou.org>
2023-03-07 10:30:29 -0800

    chapter2notes

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+for(let i=1; i<=100; i++){
+
+    if(i%5==0){
+
+    }
+        else (i%3==0){
+
+        console.log(i);
+    }
+}
--- forloops.js
+++ forloops.js
@@ -37,7 +37,7 @@
 
 let sum = 0;
 
-for(let i=0; i<=100; i++){
+for(let i=1; i<=150; i++){
 
     if(i%2==0){
 
